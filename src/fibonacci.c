71d676df00594b73e676205253647ec17a710ea5
paulvxx <89420434+paulvxx@users.noreply.github.com>
2022-09-09 16:29:15 -0600

    Update fibonacci.c

--- src/fibonacci.c
+++ src/fibonacci.c
@@ -3,12 +3,15 @@
 #include <string.h>
 
 unsigned long long fibonacci(int n) {
+    // base case #1
     if (n == 0) {
         return 0;
     }
+    // base case #2
     else if (n == 1) {
         return 1;
     }
+    // recursive function call
     else {
         return fibonacci(n-1) + fibonacci(n-2);
     }
