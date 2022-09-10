4a46af74a2e6f30e9ea4c944e574ea7bc40c84b6
kalyanbhetwal <kalyanbtl@gmail.com>
2022-09-09 19:04:18 -0600

    update

--- PA1/test/pa1_test.c
+++ PA1/test/pa1_test.c
@@ -1,8 +1,10 @@
 //
 // Created by Kalyan on 9/8/22.
 //
+
 #include "gtest/gtest.h"
 
-TEST(PA1, Fibonacci){
-    EXPECT_EQ('true','true');
+TEST(testMath, myTest)
+{
+    EXPECT_EQ(1000, 1000); 
 }
