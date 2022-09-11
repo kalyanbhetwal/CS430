1640074b95f4f4846acf006ebfa0502a7bddf76b
kalyanbhetwal <kalyanbtl@gmail.com>
2022-09-11 03:41:19 -0600

    update the latest working code

--- PA1/src/leibniz.c
+++ PA1/src/leibniz.c
@@ -57,19 +57,3 @@ double get_pi(int digits) {
 
     return f;
 }
-
-int main(int argc, char *argv[] ) {
-
-    char* strIndex = argv[1];
-    int digits = 0;
-    // convert number of digits (as string) to integer
-    if (strcmp(strIndex,"0")!=0) {
-        digits = atoi(strIndex);
-    }
-
-    // calculate necessary precisison needed
-    // for n correct digits (decimal places) of PI
-    double f = get_pi(digits);
-    printf("%.30f\n", f);
-    return 0;
-}
