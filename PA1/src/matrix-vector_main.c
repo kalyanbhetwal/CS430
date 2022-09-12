b300163a25bc93e5961b1a6041e2ceb46821252b
kalyanbhetwal <kalyanbtl@gmail.com>
2022-09-11 23:36:22 -0600

    update

---
+++ PA1/src/matrix-vector_main.c
@@ -0,0 +1,15 @@
+
+
+int main(int argc, char *argv[]){
+    if (argc < 3)
+	{
+		fprintf(stderr, "Usage: %s [martix-market-filename]\n", argv[0]);
+		exit(1);
+	}
+    struct matrixVector* result;
+    result = matrixMultiply(argv[1], argv[2]);
+    // for(int i = 0 ; i< result->nrows;i++){
+    //     printf("%lf\n", result->A[i]);
+    // }
+
+}
