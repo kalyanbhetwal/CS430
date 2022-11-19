90996bf2812bdada64c1187394a6adb77c9385ce
phillipbruce1 <phillipbruce@u.boisestate.edu>
2022-11-19 14:15:18 -0700

    Implemented monte carlo for pa3

--- PA3/src/monte-carlo.c
+++ PA3/src/monte-carlo.c
@@ -47,16 +47,17 @@ float square(float num)
  * @param maxIterations - Number of points to test
  * @return - Estimated value of pi
  */
-long long monteCarlo(long long maxIterations)
+long long monteCarlo(long long maxIterations, int thread_num)
 {
     long long hits = 0;
     // init seed for thread
-    unsigned int seed = time(0);
-    rand_r(&seed);
-    float radius = 3.0f;
+    unsigned int seed = time(0) ^ (unsigned int)thread_num;
+    float radius = 10000.0f;
     for (long long iterationsLeft = maxIterations; iterationsLeft > 0; iterationsLeft--)
     {
-        if (square(getRand(radius)) + square(getRand(radius)) <= square(radius))
+        float rand_1 = (float)(((int)rand_r(&seed) % (int)(radius * 100)) / (float)100);
+        float rand_2 = (float)(((int)rand_r(&seed) % (int)(radius * 100)) / (float)100);
+        if (rand_1 * rand_1 + rand_2 * rand_2 <= radius * radius)
         {
             hits++;
         }
@@ -72,10 +73,27 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    printf("Rank %d out of %d processors\n", world_rank, world_size);
     MPI_Status status;
-    if (world_rank == 0)
+    if (world_size == 1)
     {
+        printf("Running %ld iterations on %d thread(s)\n", strtol(argv[1], NULL, 10), world_size);
+        // get start time
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
+        long long hits = monteCarlo(strtol(argv[1], NULL, 10), world_rank);
+        gettimeofday(&end, NULL);
+        // time_t elapsed_time = end.tv_usec - start.tv_usec;
+        long int time_in_microseconds = ((end.tv_sec - start.tv_sec) * 1000000L + end.tv_usec) - start.tv_usec;
+        printf("Total time: %ld microseconds\n", time_in_microseconds);
+        printf("Or %ld milliseconds\n", time_in_microseconds / 1000);
+        printf("pi = %f\n", (float)(4 * ((float)hits / (float)strtol(argv[1], NULL, 10))));
+    }
+    else if (world_rank == 0)
+    {
+        printf("Running %ld iterations on %d thread(s)\n", strtol(argv[1], NULL, 10), world_size);
+        // get start time
+        struct timeval start, end;
+        gettimeofday(&start, NULL);
         long long totalHits = 0;
         for (int i = 1; i < world_size; i++)
         {
@@ -83,11 +101,16 @@ int main(int argc, char *argv[])
             MPI_Recv(&receivedHits, 1, MPI_LONG_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             totalHits = totalHits + receivedHits;
         }
+        gettimeofday(&end, NULL);
+        // time_t elapsed_time = end.tv_usec - start.tv_usec;
+        long int time_in_microseconds = ((end.tv_sec - start.tv_sec) * 1000000L + end.tv_usec) - start.tv_usec;
+        printf("Total time: %ld microseconds\n", time_in_microseconds);
+        printf("Or %ld milliseconds\n", time_in_microseconds / 1000);
         printf("pi = %f\n", (float)(4 * ((float)totalHits / (float)strtol(argv[1], NULL, 10))));
     }
     else
     {
-        long long hits = monteCarlo(strtol(argv[1], NULL, 10));
+        long long hits = monteCarlo(strtol(argv[1], NULL, 10) / (world_size - 1), world_rank);
         MPI_Send(&hits, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
     }
     MPI_Finalize();
