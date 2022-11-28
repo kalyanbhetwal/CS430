097395b9d744c4ab5ed9695a56075dbe0911c33f
paulvxx <89420434+paulvxx@users.noreply.github.com>
2022-11-28 15:28:03 -0700

    Update leibniz_main.c
    Added timer

--- PA3/src/leibniz_main.c
+++ PA3/src/leibniz_main.c
@@ -1,6 +1,27 @@
-#include "../include/leibniz.h"
 #include <mpi.h>
-#define N 1000
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <omp.h>
+
+
+double get_pi(int term, int lower, int upper) {
+    // compute PI to a given number of terms in the Leibniz series
+
+    double pf = 0.0; // 
+    // Calculate the partial sum of the Leibniz series from the lower bound to upper bound
+    for (int i = lower; i < (upper); i++) {
+        if (i % 2 == 0) {
+                pf += (double) 4 / (2*i+1);
+        }
+        else {
+                pf -= (double) 4 / (2*i+1);
+        }
+    }
+
+    return pf;
+}
 
 int main(int argc, char** argv) {
     int nproc, pid, i;
@@ -17,8 +38,26 @@ int main(int argc, char** argv) {
   	}
    
     if (pid==0) {
+    
+        if (argc == 1) {
+          printf("Please input a positive number\n");
+          return(-1);
+        }
+        char* strIndex = argv[1];
+        int N = 0;
+        // convert number of digits (as string) to integer
+        if (strcmp(strIndex,"0")!=0) {
+           N = atoi(strIndex);
+        }
+    
         int split = N / nproc;
         int lower, upper;
+        
+
+        
+        double start; 
+        double end;
+        start = omp_get_wtime();
         
         // processes 1 ... number of process - 1
         for (i = nproc-1; i > 0; i--) {
@@ -26,6 +65,7 @@ int main(int argc, char** argv) {
             upper = N - split*(nproc-i-1);
             MPI_Send(&upper, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&lower, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+            MPI_Send(&N, 1, MPI_INT, i, 2, MPI_COMM_WORLD);
         }
         
         // initial process get remaining work + remainder after dividing n by number of threads
@@ -40,14 +80,21 @@ int main(int argc, char** argv) {
             total += tmp;
         }
         
+        
         printf("%.30f\n", total);
+        
+        end = omp_get_wtime();
+        printf("Work took %f seconds\n", end - start);
+
     }
     else {
-       int upper, lower;
+       int upper, lower, NT;
        double tmp;
+       MPI_Recv(&NT, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+       
        MPI_Recv(&upper, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
        MPI_Recv(&lower, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-       tmp = get_pi(N, lower, upper);
+       tmp = get_pi(NT, lower, upper);
        MPI_Send(&tmp, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
     
