a420165783ae0343f9b3d9664175964031abdf06
paulvxx <89420434+paulvxx@users.noreply.github.com>
2022-12-05 12:12:38 -0700

    Update leibniz_main.c
    added program header file

--- PA3/src/leibniz_main.c
+++ PA3/src/leibniz_main.c
@@ -1,10 +1,10 @@
-#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <omp.h>
-
+#include "mpi.h"
+#include "../include/leibniz.h"
 
 double get_pi(int term, int lower, int upper) {
     // compute PI to a given number of terms in the Leibniz series
